583da836c21a45be4506c48fe8ef762471e2a192
Serge Smelov <sa.smelov@gmail.com>
2013-09-24 12:25:05 +0400

    Added what-we-do page

---
+++ what-we-do.php
@@ -0,0 +1,89 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>All for Horses - What we do</title>
+		<!--CSS-->
+		<link rel="stylesheet" href="css/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="center-container">
+		
+			<div id="header-section">		
+				<div id="logo-holder">
+					<a id="logo" href="#">
+						<img src="images/logo.png" title="All for Horses" alt="All for Horses">
+					</a>
+				</div>
+				
+				<ul id="nav">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">What we do</a></li>
+					<li><a href="#">How you can help</a></li>
+					<li><a href="#">About us</a></li>
+					<li class="donate-item rounded"><a href="#">Donate</a></li>						
+				</ul>
+			</div>
+			
+			<div class="cb"></div>
+											
+			<div id="main-section">
+			
+				<div id="main-image">
+					<img src="images/what-we-do.png" alt="what we do" title="what we do">
+					<div class="floating-text"><span>What we do</span></div>
+				</div>
+								
+				<div class="sep"></div>
+										
+				<div id="sidebar-nav">
+					<div class="section-name">WHAT WE DO</div>
+					<ul>
+						<li><a href="#">Our aims</a></li>
+						<li><a href="#">Rehabilitate</a></li>
+						<li><a href="#">Rehome</a></li>
+						<li><a href="#">Gifting your horse to 'All for Horses'</a></li>
+						<li><a href="#">Rescue stories</a></li>
+						<li><a href="#">Non-horsey services</a></li>
+					</ul>
+					
+				</div>
+				
+				<div id="main-content">
+					<h2>Our aims</h2>
+					<p>An increasing number of good, previously well-loved family riding horses and ponies are still being 
+						sent for meat or abandoned every year, and this number is rising all the time.</p> 
+					<p>While we sadly cannot save all the horses in this predicament, our aim is to take in and provide 
+						care, rehabilitation and rehoming facilities so that some of these horses can have useful lives and
+						be loved and cared for again. 
+					</p>
+					<p>
+						Despite the UK being a nation of animal lovers, ill treatment, neglect and ignorance are still part of 
+						many horses lives, and so our work also focuses on promoting cruelty-free humane training 
+						methods and horse-friendly care practices. We help do this by providing advice and information 
+						to horse owners and the public, and by organising events and horse training demonstrations.
+					</p>
+					
+					<div class="small-sep"></div>
+					
+					<h2>Rehabilitation</h2>
+					<p>Horses taken in are assessed both for health and behavioural issues and a written plan prepared for each horse, in addition to the health record. Many rescue horses have simply fallen on hard times and are in need of some TLC, and following careful monitoring will usually recover easily.</p>
+					<p>Depending on the circumstances, some have physical issues and need longer to recuperate, while others have been traumatised and need remedial training and initially expert handling to help them recover from their fears.</p>
+					<p>Owners are mostly honest about their horses’ needs and abilities, however some desperate owners have been known omit important details when they need “rid” of a horse, so it is essential that we fully assess each horse ourselves.</p>
+					<p>Once a horse is fit and well, and has received any training or treatment needed, he or she will be offered for rehoming on a long term loan basis.</p>					
+					<p>Our aim wherever possible is to provide training and  schooling so that horses go out on loan suitable  for ridden work, rather  than as non-ridden companions (although we do have companions available too), as there is  more demand  for rideable horses.</p>								
+				</div>
+				<div class="cb"></div>				
+			</div>				
+		
+		</div>
+		
+		<div id="footer-section">
+			<div class="center-container">
+				<p class="copyright">© All for Horses. Registered charity number 111111.</p>
+			</div>
+		</div>
+		
+	</body>
+</html>
