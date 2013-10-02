36e5a14327b70745a4e6d913065bceb3cc10de5c
Serge Smelov <sa.smelov@gmail.com>
2013-10-02 12:08:36 +0400

    Added main page layout

--- index.php
+++ index.php
@@ -1,9 +1,142 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>All for Horses - Charity</title>
+		<meta charset="utf-8">
+		<title>All for Horses - What we do</title>
+		<!--CSS-->
+		<link rel="stylesheet" href="css/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-		<h1>This is test</h1>
+		<div class="center-container">
+		
+			<div id="header-section">		
+				<div id="logo-holder">
+					<a id="logo" href="#">
+						<img src="images/logo.png" title="All for Horses" alt="All for Horses">
+					</a>
+				</div>
+				
+				<ul id="nav">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">What we do</a></li>
+					<li><a href="#">How you can help</a></li>
+					<li><a href="#">About us</a></li>
+					<li class="donate-item rounded"><a href="#">Donate</a></li>						
+				</ul>
+			</div>
+			
+			<div class="cb"></div>
+											
+			<div id="main-section">
+			
+				<div id="main-image">
+					<img src="images/main.png" alt="main" title="main">
+					<div class="floating-text-main"><span>Helping horses in need</span></div>
+				</div>
+								
+				<div class="sep"></div>
+				
+				<div id="services">
+					<div class="small-block">						
+						<img src="images/main-what-we-do.png" alt="what we do" title="what we do">
+						<h2>What we do</h2>
+						<p>
+							Take in, rehabilitate and rehome animals in need, in particular horses and ponies.
+						</p>						
+						<a class="link" href="#">Read more</a>
+					</div>
+					
+					<div class="small-block">						
+						<img src="images/main-how-you-can-help.png" alt="how you can help" title="how you can help">
+						<h2>How you can help</h2>
+						<p>
+							We welcome  volunteers from all walks of life. If you have  some  free time and
+							would like  to help (even if it is only a few hours a month) please get in touch
+							as we would like to hear from you.
+						</p>						
+						<a class="link" href="#">Read more</a>
+					</div>
+
+					<div class="small-block last-block">						
+						<img src="images/main-shop.png" alt="shop" title="shop">
+						<h2>Shop</h2>
+						<p>
+							Visit our online shop and help raise money for the animals.
+						</p>
+						<a class="link" href="#">Read more</a>
+					</div>
+					
+					<div class="cb"></div>
+				</div>
+				
+				<div class="tiny-sep"></div>
+				
+				<div class="left-col-big">
+					<h2>Rescue stories</h2>
+				
+					<div class="post">
+						<img src="images/stories/story1.png" alt="story1" title="story1">
+						<h3>Duchess the magnificent Clydesdale</h3>
+						<p>Duchess, a teenage Clydesdale mare, was brought to us by a caring young girl who had bought Duchess from a dealer who kept her tethered at a travellers site, and given her a good home.</p>
+						<a class="link" href="#">Read more</a>
+					</div>
+				
+					<div class="post">
+						<img src="images/stories/story2.png" alt="story2" title="story2">
+						<h3>Pippa’s story</h3>
+						<p>Pippa is a very pretty and lively 5 year old Welsh pony. She came into our care 2 years ago, when she was advertised on the internet as a ”project” pony.</p>
+						<a class="link" href="#">Read more</a>
+					</div>				
+								
+				</div>
+				
+				<div class="right-col-small">
+					<h2>Rehome a horse</h2>
+					<div class="horse-info">
+						<div class="horse-image"><img src="images/rehoming/horse1.png" alt="horse1" title="horse1"></div>
+						<div class="horse-details">
+							<h3>Oliver</h3>
+							<p>Welsh pony</p>
+							<p>2 years old</p>
+						</div>
+						<div class="cb"></div> 
+					</div>
+					
+					<div class="horse-info">
+						<div class="horse-image"><img src="images/rehoming/horse1.png" alt="horse1" title="horse1"></div>
+						<div class="horse-details">
+							<h3>Oliver</h3>
+							<p>Welsh pony</p>
+							<p>2 years old</p>
+						</div>
+						<div class="cb"></div>
+					</div>
+
+					<div class="horse-info">
+						<div class="horse-image"><img src="images/rehoming/horse1.png" alt="horse1" title="horse1"></div>
+						<div class="horse-details">
+							<h3>Oliver</h3>
+							<p>Welsh pony</p>
+							<p>2 years old</p>
+						</div>
+						<div class="cb"></div>
+					</div>
+					
+					<a class="link" href="#">Show all</a>
+					
+				</div>					
+			
+				<div class="cb"></div>
+			</div>				
+		
+		</div>
+		
+		<div id="footer-section">
+			<div class="center-container">
+				<p class="copyright">© All for Horses. Registered charity number 111111.</p>
+			</div>
+		</div>
+		
 	</body>
 </html>
